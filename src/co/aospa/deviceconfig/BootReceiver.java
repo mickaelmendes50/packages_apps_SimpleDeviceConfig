ProtonAOSP/android_packages_apps_SimpleDeviceConfig

SimpleDeviceConfig: Add initial implementation
This is a simple app that provisions the DeviceConfig settings namespace
on boot with values defined in a resource overlay. It is meant to be
delegated as the device configurator in the framework config
(config_deviceConfiguratorPackageName) to make sure it receives the
correct permissions.

This can be useful for e.g. configuring proprietary Google services
without delegating GMS as the device configurator to prevent it from
overriding custom default settings, or minimizing changes to core
framework code by making changes in DeviceConfig instead as intended by
Google.

src/org/protonaosp/deviceconfig/BootReceiver.java
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2007 The Proton AOSP Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.protonaosp.deviceconfig;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.DeviceConfig;
+import android.util.Log;
+
+public class BootReceiver extends BroadcastReceiver {
+    private static final String TAG = "SimpleDeviceConfig";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        new Thread(() -> {
+            Log.i(TAG, "Updating device config at boot");
+            updateConfig(context);
+        }).start();
+    }
+
+    private void updateConfig(Context context) {
+        // Set current properties
+        String[] rawProperties = context.getResources().getStringArray(R.array.device_config);
+        for (String property : rawProperties) {
+            String[] kv = property.split("=");
+            String fullKey = kv[0];
+            String[] nk = fullKey.split("/");
+
+            String namespace = nk[0];
+            String key = nk[1];
+            String value = kv[1];
+
+            DeviceConfig.setProperty(namespace, key, value, true);
+        }
+    }
+}

mickaelmendes50/packages_apps_SimpleDeviceConfig

Signed-off-by: Mr Mesquita <mickaelmendes50@gmail.com>

src/co/aospa/deviceconfig/BootReceiver.java
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2007 The Proton AOSP Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.aospa.deviceconfig;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.DeviceConfig;
+import android.util.Log;
+
+public class BootReceiver extends BroadcastReceiver {
+    private static final String TAG = "SimpleDeviceConfig";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        new Thread(() -> {
+            Log.i(TAG, "Updating device config at boot");
+            updateConfig(context);
+        }).start();
+    }
+
+    private void updateConfig(Context context) {
+        // Set current properties
+        String[] rawProperties = context.getResources().getStringArray(R.array.device_config);
+        for (String property : rawProperties) {
+            String[] kv = property.split("=");
+            String fullKey = kv[0];
+            String[] nk = fullKey.split("/");
+
+            String namespace = nk[0];
+            String key = nk[1];
+            String value = kv[1];
+
+            DeviceConfig.setProperty(namespace, key, value, true);
+        }
+    }
+}